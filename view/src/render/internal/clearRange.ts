2739fd3f21b4c18dc5c765f87caee91acc3767d9
Andrew K <andrewjk@users.noreply.github.com>
2024-08-05 14:47:22 +1000

    Fix: delete ranges with document ranges

--- view/src/render/internal/clearRange.ts
+++ view/src/render/internal/clearRange.ts
@@ -2,24 +2,15 @@ import type Range from "../../global/Range";
 import removeRangeEffects from "../../watch/internal/removeRangeEffects";
 
 export default function clearRange(range: Range) {
-  // NOTE: range.deleteContents hangs in JSDOM (or at least in benching/testing with JSDOM),
-  // so we need to do our own deleting
+  const docRange = document.createRange();
   if (range.startNode) {
-    range.endNode = range.endNode || range.startNode;
-
-    const parent = range.startNode.parentNode!;
-    let currentNode = range.startNode;
-    while (currentNode !== range.endNode) {
-      let nextNode = currentNode!.nextSibling;
-      //console.log("cleared", currentNode);
-      parent.removeChild(currentNode!);
-      currentNode = nextNode!;
-    }
-    parent.removeChild(currentNode);
+    let endNode = range.endNode || range.startNode;
+    docRange.setStartBefore(range.startNode);
+    docRange.setEndAfter(endNode);
+    docRange.deleteContents();
+    range.startNode = null;
+    range.endNode = null;
   }
-
-  delete range.startNode;
-  delete range.endNode;
 
   // TODO: Put this somewhere better?
   removeRangeEffects(range);
