0ce3f8c987b9ff43f8491c6eb627fc0a62fe4c6a
Andrew K <andrewjk@users.noreply.github.com>
2024-08-09 15:56:20 +1000

    Fix: slightly better parent checking

--- view/src/render/internal/addFragment.ts
+++ view/src/render/internal/addFragment.ts
@@ -3,7 +3,7 @@ import context from "../../global/context";
 export default function addFragment(
   fragment: DocumentFragment,
   parent: ParentNode,
-  before?: HTMLElement,
+  before: Node | null,
 ) {
   // TODO: Ranges for components, so that we always have an activerange
   const range = context.activeRange;
@@ -13,7 +13,15 @@ export default function addFragment(
     range.endNode = endNode !== range.startNode ? endNode : null;
   }
 
-  // HACK:
-  let parent2 = before && before.parentNode ? before.parentNode : parent;
-  parent2.insertBefore(fragment, before || null);
+  // HACK: We need to be able to add fragments to new fragments as well as
+  // fragments that have already been added to the document. New fragments
+  // will be ok, but fragments that have been added will not be parents of the
+  // before element (because it's now in the document). In this case we will
+  // have to use the before element's parent, which should work as long as we
+  // are always passing a before element...
+  if (parent.nodeType === 11) {
+    parent = before!.parentNode!;
+  }
+
+  parent.insertBefore(fragment, before);
 }
