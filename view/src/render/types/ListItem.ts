9fe7e3a47c25e178248af8c984f3184d36b37f07
Andrew K <andrewjk@users.noreply.github.com>
2024-08-14 17:36:30 +1000

    Refactor: remove unused property

--- view/src/render/types/ListItem.ts
+++ view/src/render/types/ListItem.ts
@@ -3,5 +3,5 @@ import type Range from "../../global/types/Range";
 export default interface ListItem extends Range {
   key: any;
   data: Record<string, any>;
-  range: Range;
+  //range: Range;
 }
