ae8b2ec5782eb2b3f95484733cc78958b315b1f7
Andrew K <andrewjk@users.noreply.github.com>
2024-08-23 13:42:43 +1000

    Feat: $mount for delaying $run until after component mount

--- view/src/index.ts
+++ view/src/index.ts
@@ -2,10 +2,11 @@ import build from "./compile/build";
 import parse from "./compile/parse";
 import hydrate from "./render/hydrate";
 import mount from "./render/mount";
+import $mount from "./watch/$mount";
 import $run from "./watch/$run";
 import $unwrap from "./watch/$unwrap";
 import $watch from "./watch/$watch";
 
 // This is used as an input to tsconfig
 
-export { parse, build, mount, hydrate, $watch, $unwrap, $run };
+export { parse, build, mount, hydrate, $watch, $unwrap, $run, $mount };
