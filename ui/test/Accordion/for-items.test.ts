7e1d3a6109d9066ce48394345759d38264e10a4c
Andrew K <andrewjk@users.noreply.github.com>
2024-09-12 16:49:12 +1000

    Test: dynamic accordion items

---
+++ ui/test/Accordion/for-items.test.ts
@@ -0,0 +1,55 @@
+import { getByText, queryByText } from "@testing-library/dom";
+import "@testing-library/jest-dom/vitest";
+import userEvent from "@testing-library/user-event";
+import { describe, expect, it } from "vitest";
+import $watch from "../../../view/src/$watch";
+import mount from "../../../view/src/mount";
+import AccordionList from "./components/AccordionList.tera";
+
+describe("Accordion", () => {
+	it("Items in a for loop", async () => {
+		const user = userEvent.setup();
+
+		let $state = $watch({
+			value: "1",
+			items: [
+				{ id: 0, header: "Item 1", text: "Content 1" },
+				{ id: 1, header: "Item 2", text: "Content 2" },
+				{ id: 2, header: "Item 3", text: "Content 3" },
+			],
+		});
+
+		const container = document.createElement("div");
+		document.body.appendChild(container);
+		mount(container, AccordionList, $state);
+
+		expect(queryByText(container, "Content 1")).not.toBeInTheDocument();
+		expect(queryByText(container, "Item 2")).toBeInTheDocument();
+		expect(queryByText(container, "Content 2")).toBeInTheDocument();
+		expect(queryByText(container, "Content 3")).not.toBeInTheDocument();
+
+		$state.items.splice(1, 1);
+
+		expect(queryByText(container, "Content 1")).not.toBeInTheDocument();
+		expect(queryByText(container, "Item 2")).not.toBeInTheDocument();
+		expect(queryByText(container, "Content 2")).not.toBeInTheDocument();
+		expect(queryByText(container, "Content 3")).not.toBeInTheDocument();
+
+		// Clicking header 1 should reveal body 1
+		await user.click(getByText(container, "Item 1"));
+		expect(queryByText(container, "Content 1")).toBeInTheDocument();
+
+		$state.items.push({ id: 3, header: "Item 4", text: "Content 4" });
+
+		expect(queryByText(container, "Content 1")).toBeInTheDocument();
+		expect(queryByText(container, "Content 2")).not.toBeInTheDocument();
+		expect(queryByText(container, "Content 3")).not.toBeInTheDocument();
+		expect(queryByText(container, "Item 4")).toBeInTheDocument();
+		expect(queryByText(container, "Content 4")).not.toBeInTheDocument();
+
+		// Clicking header 4 should reveal body 4
+		await user.click(getByText(container, "Item 4"));
+		expect(queryByText(container, "Content 1")).not.toBeInTheDocument();
+		expect(queryByText(container, "Content 4")).toBeInTheDocument();
+	});
+});
