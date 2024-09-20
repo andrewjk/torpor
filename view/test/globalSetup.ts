fc3b40b8bb8466c682f1196f599ed0ed777bde3b
Andrew K <andrewjk@users.noreply.github.com>
2024-09-20 14:26:42 +1000

    Refactor: extract creating files from hydrateComponent

--- view/test/globalSetup.ts
+++ view/test/globalSetup.ts
@@ -3,3 +3,11 @@ import "../scripts/buildTestOutput";
 export async function setup() {
 	// Build the output files in the above import
 }
+
+//export async function teardown() {
+//	// Delete all of the *.server.tera files that we created while hydrating
+//	const files = await fg("**/*.server.tera", {
+//		absolute: true,
+//	});
+//	await Promise.all(files.map((f) => fs.unlink(f)));
+//}
