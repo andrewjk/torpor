8de094b2f7cbb23676b8885442edcb3d7d0a139d
Andrew K <andrewjk@users.noreply.github.com>
2025-04-17 14:21:47 +1000

    Fix: ignore trailing slashes when matching routes

---
+++ .changeset/ten-ears-grin.md
@@ -0,0 +1,5 @@
+---
+"@torpor/build": patch
+---
+
+Fix: ignore trailing slashes when matching routes
--- packages/build/src/site/pathToRegex.ts
+++ packages/build/src/site/pathToRegex.ts
@@ -1,10 +1,15 @@
 export default function pathToRegex(path: string): RegExp {
-	const pattern = path
-		.split("/")
-		.map((p) => {
-			return p.replace(/\[([^\/]+?)\]/, "(?<$1>[^\\/]+?)");
-		})
-		.join("\\/")
-		.replaceAll("*", ".*");
-	return new RegExp(`^${pattern}$`);
+	const pattern =
+		path
+			.split("/")
+			// Replace `[slug]` with a grouped match so we can pull out params
+			.map((p) => {
+				return p.replace(/\[([^\/]+?)\]/, "(?<$1>[^\\/]+?)");
+			})
+			.join("\\/")
+			// Replace globs with regex syntax
+			.replaceAll("*", ".*") +
+		// Ignore trailing slashes -- `/path` should be treated the same as `/path/`
+		"/*";
+	return new RegExp(`^${pattern}/*$`);
 }
