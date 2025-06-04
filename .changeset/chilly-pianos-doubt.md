2d7fae69b7f7ee7f831fb5d92ac5c51627f10799
Andrew K <andrewjk@users.noreply.github.com>
2025-06-04 15:10:04 +1000

    Fix: show the first tab content if no value set

---
+++ .changeset/chilly-pianos-doubt.md
@@ -0,0 +1,5 @@
+---
+"@torpor/ui": patch
+---
+
+Fix: show the first tab content if no value set
--- packages/ui/test/TabGroup/item-trigger.test.ts
+++ packages/ui/test/TabGroup/item-trigger.test.ts
@@ -11,22 +11,23 @@ describe("TabGroup", () => {
 		document.body.appendChild(container);
 		mount(container, TabGroupTrigger);
 
-		expect(queryByText(container, "Content 1")).not.toBeInTheDocument();
+		expect(queryByText(container, "Content 1")).toBeInTheDocument();
 		expect(queryByText(container, "Content 2")).not.toBeInTheDocument();
 		expect(queryByText(container, "Content 3")).not.toBeInTheDocument();
 
-		// Clicking header 1 should reveal body 1
-		await userEvent.click(getByText(container, "Header 1"));
-		expect(queryByText(container, "Content 1")).toBeInTheDocument();
-
-		// Clicking header 2 should hide body 1 and reveal body 2
-		await userEvent.click(getByText(container, "Header 2"));
+		// Clicking header 3 should hide body 1 and reveal body 3
+		await userEvent.click(getByText(container, "Header 3"));
 		expect(queryByText(container, "Content 1")).not.toBeInTheDocument();
+		expect(queryByText(container, "Content 3")).toBeInTheDocument();
+
+		// Clicking header 2 should hide body 3 and reveal body 2
+		await userEvent.click(getByText(container, "Header 2"));
+		expect(queryByText(container, "Content 3")).not.toBeInTheDocument();
 		expect(queryByText(container, "Content 2")).toBeInTheDocument();
 
 		// Clicking header 2 again should keep body 2
 		await userEvent.click(getByText(container, "Header 2"));
-		expect(queryByText(container, "Content 1")).not.toBeInTheDocument();
+		expect(queryByText(container, "Content 3")).not.toBeInTheDocument();
 		expect(queryByText(container, "Content 2")).toBeInTheDocument();
 	});
 });
