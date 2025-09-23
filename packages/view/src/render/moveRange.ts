090051da422a9f6dd5eba5d97320cafc52c141ca
Andrew K <andrewjk@users.noreply.github.com>
2025-09-23 17:35:36 +1000

    Perf: use the anchor node's parent when we can

--- packages/view/src/render/moveRange.ts
+++ packages/view/src/render/moveRange.ts
@@ -1,16 +1,9 @@
 import type Range from "../types/Range";
-import isFragmentNode from "./isFragmentNode";
 
 export default function moveRange(parent: Node, range: Range, before: ChildNode | null): void {
+	parent = before?.parentNode ?? parent;
 	const endNode = range.endNode ?? range.startNode;
 	let currentNode = range.startNode;
-
-	// HACK: The parent may have once been a document fragment, but will now be
-	// in the document, so we will have to use the before element's parent,
-	// which should work as long as we are always passing a before element...
-	if (isFragmentNode(parent)) {
-		parent = before!.parentNode!;
-	}
 
 	while (currentNode !== null) {
 		const nextNode = currentNode.nextSibling;
