aa148ad6f3266e0dd5cb8bb36234e0011deb0842
Andrew K <andrewjk@users.noreply.github.com>
2025-11-19 12:16:21 +1100

    Chore: also publish files in the src folder

---
+++ .changeset/breezy-mirrors-deny.md
@@ -0,0 +1,5 @@
+---
+"@torpor/build": patch
+---
+
+Chore: also publish files in the src folder
--- packages/build/tsdown.config.ts
+++ packages/build/tsdown.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
 		"src/state.ts",
 		"src/test.ts",
 		"src/bin/index.ts",
+		// TODO: Compile these so we don't have to include the entire src folder in the build
+		//"src/site/clientEntry.ts",
+		//"src/site/clientEntryDev.ts",
+		//"src/site/serverEntry.ts",
 	],
 	// Put this in here to stop issues with bundling Vite from bin/index.ts
 	// I'm not sure if this will cause further issues down the line?
