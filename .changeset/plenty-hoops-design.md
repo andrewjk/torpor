2be9d7513a208bd2e1e51c6e7dc0d8d082244e24
Andrew K <andrewjk@users.noreply.github.com>
2025-11-26 17:33:29 +1100

    Fix: handle Symbols and Proxies in text

---
+++ .changeset/plenty-hoops-design.md
@@ -0,0 +1,5 @@
+---
+"@torpor/view": patch
+---
+
+Fix: handle Symbols and Proxies in text
--- packages/view/src/render/formatText.ts
+++ packages/view/src/render/formatText.ts
@@ -1,3 +1,8 @@
+import $unwrap from "../watch/$unwrap";
+
 export default function formatText(value: any): string {
-	return value ?? "";
+	// We could have anything in value, so we need to
+	// - use String(...) because Symbols can't be implicitly converted
+	// - use $unwrap because Proxies can't be converted to a string
+	return value === undefined || value === null ? "" : String($unwrap(value));
 }
