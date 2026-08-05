bb4dc6bc393ce2e59a13a626238a5a14698e86f7
Andrew K <andrewjk@users.noreply.github.com>
2026-08-05 10:44:06 +1000

    Chore: update dependencies

--- packages/build/src/server/CookieHelper.ts
+++ packages/build/src/server/CookieHelper.ts
@@ -1,4 +1,4 @@
-import { SerializeOptions, parse, serialize } from "cookie";
+import { Cookies, SerializeOptions, parseCookie, stringifyCookie } from "cookie";
 
 export default class CookieHelper {
 	request: Request;
@@ -11,26 +11,35 @@ export default class CookieHelper {
 	get(name: string): string | undefined {
 		// If it's been set, return the value from the map
 		if (this.cookies.has(name)) {
-			return parse(this.cookies.get(name)!)[name];
+			return parseCookie(this.cookies.get(name)!)[name];
 		}
+
 		// Otherwise, return the value from the `cookie` header
 		const header = this.request.headers.get("Cookie");
-		return parse(header ?? "")[name];
+		return parseCookie(header ?? "")[name];
 	}
 
 	set(name: string, value: string, options?: SerializeOptions): void {
+		const c: Cookies = {}
+		c[name] = value;
+
 		// Set a cookie header with some default values for security
 		options ??= {};
 		options.httpOnly ??= true;
 		options.secure ??= true;
 		options.sameSite ??= "lax";
-		this.cookies.set(name, serialize(name, value, options));
+
+		this.cookies.set(name, stringifyCookie(c, options));
 	}
 
 	delete(name: string, options?: SerializeOptions): void {
+		const c: Cookies = {}
+		c[name] = "";
+
 		// Set a cookie header with an immediate expiry date
 		options ??= {};
 		options.expires = new Date(0);
-		this.cookies.set(name, serialize(name, "", options));
+
+		this.cookies.set(name, stringifyCookie(c, options));
 	}
 }
