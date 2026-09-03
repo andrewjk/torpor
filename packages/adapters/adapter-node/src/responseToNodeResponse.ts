e2c0c81ee2b80721dfc1f02ebfcff6828c9d06af
Andrew K <andrewjk@users.noreply.github.com>
2026-09-03 10:26:31 +1000

    Perf: don't set unnecessary status text

--- packages/adapters/adapter-node/src/responseToNodeResponse.ts
+++ packages/adapters/adapter-node/src/responseToNodeResponse.ts
@@ -7,44 +7,42 @@ export default async function responseToNodeResponse(
 	res: Response,
 	outgoing: ServerResponse | Http2ServerResponse,
 ): Promise<void> {
-	outgoing.statusCode = res.status;
-	outgoing.statusMessage = res.statusText;
-
 	res.headers.forEach((value, key) => {
 		outgoing.setHeader(key, value);
 	});
 
 	const body = (res as any)[INTERNAL_BODY];
 
+	// `writeHead(status)` uses node's standard reason phrase for the code
+	// (the same one undici's `statusText` carries)
 	if (body === null || body === undefined) {
 		// @ts-ignore
-		outgoing.writeHead(res.status, res.statusText);
+		outgoing.writeHead(res.status);
 		outgoing.end();
 	} else if (typeof body === "string" || body instanceof Uint8Array) {
 		// @ts-ignore
-		outgoing.writeHead(res.status, res.statusText);
+		outgoing.writeHead(res.status);
 		outgoing.end(body);
 	} else if (body instanceof Blob) {
 		// @ts-ignore
-		outgoing.writeHead(res.status, res.statusText);
+		outgoing.writeHead(res.status);
 		outgoing.end(new Uint8Array(await body.arrayBuffer()));
 	} else if (body instanceof ArrayBuffer) {
 		// @ts-ignore
-		outgoing.writeHead(res.status, res.statusText);
+		outgoing.writeHead(res.status);
 		outgoing.end(new Uint8Array(body));
 	} else if (isArrayBufferView(body)) {
 		// @ts-ignore
-		outgoing.writeHead(res.status, res.statusText);
+		outgoing.writeHead(res.status);
 		outgoing.end(new Uint8Array(body.buffer));
 	} else if (body instanceof FormData) {
-		// TODO
 		outgoing.setHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
 		// @ts-ignore
-		outgoing.writeHead(res.status, res.statusText);
+		outgoing.writeHead(res.status);
 		outgoing.end();
 	} else {
 		// @ts-ignore
-		outgoing.writeHead(res.status, res.statusText);
+		outgoing.writeHead(res.status);
 		outgoing.end();
 	}
 }
